52a54e1c619060f97a2afec4a9a65ea898825c6e
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2026-08-12 14:11:00 +0200

    feature: add constants package

---
+++ constants/constants.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+	VshBasePath         = "/usr/local/valet-sh/valet-sh"
+	VshVenvPath         = "/usr/local/valet-sh/venv"
+	VshAnsibleFactsFile = "/tmp/ansible-facts/local"
+)
