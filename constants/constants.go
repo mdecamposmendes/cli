0dc258e6d36bafcd17454e0e777fc42be103d032
Maximilian de Campos Mendes <m.decamposmendes@techdivision.com>
2026-08-12 14:42:26 +0200

    refactor: update constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,7 +1,13 @@
 package constants
 
 const (
-	VshBasePath         = "/usr/local/valet-sh/valet-sh"
-	VshVenvPath         = "/usr/local/valet-sh/venv"
-	VshAnsibleFactsFile = "/tmp/ansible-facts/local"
+	VshRootPath               = "/usr/local/valet-sh"
+	VshBasePath               = VshRootPath + "/valet-sh"
+	VshVenvPath               = VshRootPath + "/venv"
+	VshEtcPath                = VshRootPath + "/etc"
+	VshAnsibleFactsFile       = "/tmp/ansible-facts/local"
+	VshServiceFile            = VshEtcPath + "/services.yml"
+	VshBundlesFile            = VshEtcPath + "/bundles.yml"
+	VshReleaseChannelFile     = "RELEASE_CHANNEL"
+	VshReleaseChannelFilePath = VshEtcPath + "/" + VshReleaseChannelFile
 )
